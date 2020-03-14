b4f05a4f09067f005874befb94825bf0d565601e
alyabozied <alyweeka99@gmail.com>
2020-03-14 20:20:54 +0200

    Updates

---
+++ precache-manifest.dab082f620a0f3e627239266d965a52c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c9564cea89637110feb62baac915ff",
+    "url": "/react-games/index.html"
+  },
+  {
+    "revision": "e296f49cbcdcf7d0aa94",
+    "url": "/react-games/static/css/main.ea8f59b3.chunk.css"
+  },
+  {
+    "revision": "31fa90c446e00746a924",
+    "url": "/react-games/static/js/2.c199f3cd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-games/static/js/2.c199f3cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e296f49cbcdcf7d0aa94",
+    "url": "/react-games/static/js/main.30a07a42.chunk.js"
+  },
+  {
+    "revision": "b857469308244d1ac060",
+    "url": "/react-games/static/js/runtime-main.e458b188.js"
+  }
+]);
